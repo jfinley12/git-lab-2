65f04bd79411dafa59fd223782f831c4e531b4d0
jfinley12 <113718288+jfinley12@users.noreply.github.com>
2022-10-05 19:12:52 -0400

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Enter your name>
+ *        Date: <Enter today's date>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*Jacob Finley 10/5/22*/
+  cout << "Hello Git!" << endl;
+    return 0;
+}// main
